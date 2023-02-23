571d0769d4ab0277733b62d2022c13f81a0b4bc1
jiexu <linj121@mcmaster.ca>
2023-02-22 19:02:22 -0500

    Multithreading added for server.py. Error detection added for both server/client

--- client.py
+++ client.py
@@ -4,56 +4,74 @@ from pathlib import Path
 import re
 import errno
 
-HOST: str = ""
-PORT: int
 
-
-def start_client(host: str, port: int) -> str:
+def start_client(host: str, port: int, handle_data, **kwargs) -> str:
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((host, port))
         # s.sendall(b"Hello World")
-        file_data = s.recv(1024).decode()
-
-    print(f"Received:\n{file_data!r}\nEOF")
-
-    return file_data
+        kwargs["data"] = s.recv(1024).decode()
+        print("Connection closed")
+    handle_data(**kwargs)
 
 
-arguments = sys.argv[1:]
-
-if len(arguments) == 2:
-    print("Number of arguments is 2 [OK]")
-    address = arguments[0]
+def parse_addr(address: str) -> tuple:
     # ipv4 addr OR hostname validation, port number between 49152 - 65535
-    if match := re.match(r"^([a-zA-Z0-9\-\.]+)(?::(\d{1,5}))$", address):
-        HOST = match.group(1)
-        PORT = int(match.group(2))
-        print(f"Provided with HOST: {HOST}, and PORT: {PORT}")
-        ipv4_hostname_regex = r"^(([a-zA-Z]|[a-zA-Z][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z]|[A-Za-z][A-Za-z0-9\-]*[A-Za-z0-9])$ \
-                               |^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$"
-        if not re.match(ipv4_hostname_regex, HOST):
-            print(f"ERR: Invalid host: {HOST}")
-        elif not 49152 <= int(PORT) <= 65535:
-            print(f"ERR: Invalid port number: {PORT}")
+    if match := re.match(r"^([a-zA-Z0-9\-\.]+):(\d{1,5})$", address):
+        input_host = match.group(1)
+        input_port = int(match.group(2))
+        ipv4_hostname_regex = \
+            r"^((?:(?:[a-zA-Z]|[a-zA-Z][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*(?:[A-Za-z]|[A-Za-z][A-Za-z0-9\-]*[A-Za-z0-9]))$|"\
+            r"^((?:(?:25[0-5]|(?:2[0-4]|1\d|[1-9]|)\d)\.?\b){4})$"
+        if r := re.match(ipv4_hostname_regex, input_host):
+            host = r.group(2) if r.group(2) is not None else socket.gethostbyname(r.group(1))
+        else:
+            print(f"ERR: Invalid host: {input_port}")
+            exit(0)
+        if 49152 <= int(input_port) <= 65535:
+            port = input_port
+        else:
+            print(f"ERR: Invalid port number: {input_port}")
+            exit(0)
+        return host, port
     else:
         print(f"ERR: Invalid server address: {address}")
         exit(0)
-    file_name = arguments[1]
-    file_path = Path.cwd() / 'output_file' / file_name
-else:
-    print(f"ERR: 2 argument expected, but {len(arguments)} given!")
-    exit(0)
 
-data = start_client(HOST, PORT)
 
-try:
-    with open(file_path, "w") as output:
-        output.write(data)
-except IOError as x:
-    print('error ', x.errno, ',', x.strerror)
-    if x.errno == errno.EACCES:
-        print(file_path, 'no permissions')
-    elif x.errno == errno.EISDIR:
-        print(file_path, 'is directory')
+def write_to_file(path: str, data: str):
+
+    try:
+        with open(path, "w") as output:
+            output.write(data)
+        print(f"Data written to: {path}")
+    except IOError as x:
+        print('error ', x.errno, ',', x.strerror)
+        if x.errno == errno.EACCES:
+            print(path, 'no permissions')
+        elif x.errno == errno.EISDIR:
+            print(path, 'is directory')
+
+
+if __name__ == "__main__":
+
+    HOST: str = ""
+    PORT: int
+    FILE_PATH: Path
+
+    arguments = sys.argv[1:]
+    if len(arguments) == 2:
+        print("Number of arguments is 2 [OK]")
+        HOST, PORT = parse_addr(arguments[0])
+        print(f"Provided HOST: {HOST}, PORT: {PORT} [OK]")
+        FILE_PATH = Path.cwd() / 'output_file' / arguments[1]
+    else:
+        print(f"ERR: 2 argument expected, but {len(arguments)} given!")
+        exit(0)
+
+    if FILE_PATH.is_file():
+        start_client(HOST, PORT, write_to_file, path=FILE_PATH)
+    else:
+        print(f"ERR: File {FILE_PATH} doesn't exist")
+        exit(0)
 
--- server.py
+++ server.py
@@ -1,52 +1,65 @@
 import sys
-import os
+import threading
 from pathlib import Path
 import socket
 
-HOST = "127.0.0.1"
-PORT = 49152
+HOST: str = socket.gethostbyname("localhost")
+PORT: int = 50000
 
 
-def start_server(file_data: str, port: int, host: str = ''):
+def get_data_if_valid(file_path: Path) -> str:
+    try:
+        print(f"Checking for the existence of file {file_path} ...")
+        with open(file_path, "r") as input_file:
+            print(f"File {file_path} exists [OK]")
+            content = input_file.read()
+            if len(content) <= 80:
+                return content
+            else:
+                print(f"ERR: number of char exceeds 80, {len(content)} given")
+                exit(0)
+    except FileNotFoundError:
+        print(f"ERR: File {file_path} does not exist!")
+        exit(0)
+
+
+def on_new_client(client_socket: socket, addr: tuple, data: str):
+    with client_socket as s:
+        s.sendall(data.encode())
+        print(f"Data successfully sent to {addr} [OK]")
+
+
+def start_server(host: str, port: int, data: str):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((host, port))
         s.listen()
-        print(f"Server started, listening on {HOST}:{PORT}")
-        print(f"Waiting for connection...")
-        # accept() returns a socket object and a tuple containing the address
-        # If it's ipv4, the tuple is (host, port)
-        # If it's ipv6, the tuple is (host, port, flowinfo, scopeid)
-        conn, addr = s.accept()
-        # The with statement ensures the socket will be closed
-        with conn:
-            #     print('Connected by', addr)
-            #     while True:
-            #         # recv() is a blocking call
-            #         # Blocking calls have to wait on system calls (I/O) to \
-            #         # complete before they can return a value.
-            #         data = conn.recv(1024)
-            #         print(f"Received data: {data}")
-            #         if not data:
-            #             break
-            conn.sendall(file_data.encode())
+        print(f"Server started, listening on {host}:{port}")
+        while True:
+            print(f"Waiting for connection...")
+            conn = None
+            try:
+                # s.accept(): tuple -> (socket, (host, port))
+                conn, addr = s.accept()
+                print(f"Connected from {addr}")
+                thread = threading.Thread(target=on_new_client, args=(conn, addr, data))
+                thread.start()
+            except KeyboardInterrupt:
+                if conn:
+                    conn.close()
+                break
 
 
-arguments = sys.argv[1:]
+if __name__ == "__main__":
 
-if len(arguments) == 1:
-    print("Number of arguments is 1 [OK]")
-    file_name = arguments[0]
-    file_path = Path.cwd() / 'input_file' / file_name
-else:
-    print(f"ERR: 1 argument expected, but {len(arguments)} given!")
-    exit(0)
+    arguments = sys.argv[1:]
+    if len(arguments) == 1:
+        print("Number of arguments is 1 [OK]")
+        FILE_PATH = Path.cwd() / 'input_file' / arguments[0]
 
-try:
-    print(f"Checking for the existence of file {file_name} ...")
-    with open(file_path, "r") as input_file:
-        print(f"File {file_name} exists [OK]")
-        data = input_file.read()
-    start_server(data, PORT, HOST)
-except FileNotFoundError:
-    print(f"ERR: File {file_name} does not exist!")
-    exit(0)
+    else:
+        print(f"ERR: 1 argument expected, but {len(arguments)} given!")
+        exit(0)
+
+    DATA = get_data_if_valid(FILE_PATH)
+
+    start_server(HOST, PORT, DATA)
